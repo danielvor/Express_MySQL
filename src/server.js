dbcbbd4b7c43a63906d27acfa9dada967191fb40
Daniel Rodrigues <this.daniel@outlook.com>
2023-05-17 22:20:19 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const routes = require('./routes/renda.js');
-const despesas = require('./routes/despesas.js');
+const routes = require('./routes/rendaRoutes.js');
+const despesas = require('./routes/despesasRoutes.js');
 
 const app = express();
 
+app.use(express.static('public'))
 app.use(express.json());
 app.use(routes);
-app.use(despesas)
+app.use(despesas);
 
 app.listen(3000, () => {
     console.log("API está conectada na porta 3000")
 });
+
+
